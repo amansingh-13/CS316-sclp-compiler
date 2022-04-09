7a58b59f4ce47375b280200b96ab217244a48a4b
aman <amansingh@cse.iitb.ac.in>
2022-04-09 22:56:03 +0530

    one last time?

---
+++ A5/group7/file.c
@@ -0,0 +1,62 @@
+void main();
+int x, g;
+
+
+void main(){
+    int x, f;
+	int b;
+//    if(x!=0)
+//    if(x==0)
+//    if(x!=0)
+//    if(x==0)
+//    if(x!=0)
+//    if(x==0)
+//    if(x!=0)
+//    if(x!=0)
+//    if(x==0)
+//    if(x!=0)
+//    if(x==0)
+//    x = 5;
+//    else x=1;
+//    else x=1;
+//    else x=1;
+//    else x=1;
+//    else x=1;
+//    else x=1;
+//    else x=1;
+//    
+//    while(x>2)
+//    if(x==2?x>2:x<f)
+//    f = 1;
+//    else 
+//    f = 2;
+
+//    do 
+//    x = 5;
+//    while (x != 5);
+//
+//    while(x > 5){
+//        x = x*x;
+//        do {
+//            if(x<12){
+//                x = 9;
+//            }
+//            else if (x>12) {
+//                x = 18;
+//                {
+//                    x = 78;
+//                }
+//            }
+//            else {
+//                {
+//                    {
+//                        x = 90;
+//                    }
+//                }
+//                print x;
+//            }
+//        }
+//        while (x != 0);
+//        print x;
+//    }
+}
